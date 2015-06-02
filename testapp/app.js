91d1e8e5f275bd28d3b1424deeed8748a73de5a2
unknown <Dzmitry_Butsyanau@EPBYMINW1485.minsk.epam.com>
2015-06-02 15:27:12 +0300

    initial commit

---
+++ testapp/app.js
@@ -0,0 +1,16 @@
+angular.module("MyNewApp",[])
+		.controller("MyNewController",["$scope", "greetingService", function MyNewController($scope, greetingService){
+			this.greeting = greetingService;
+
+			this.clickMe = function(){
+				alert("clicked");
+			};
+		}])
+		.controller("MyNewController2",["$scope", "greetingService", function MyNewController($scope, greetingService){
+			this.greeting2 = greetingService;
+		}])
+		.factory("greetingService",[ function GreetingService(){
+			return {
+				message:"Hello from greeting service"
+			};
+		}])
